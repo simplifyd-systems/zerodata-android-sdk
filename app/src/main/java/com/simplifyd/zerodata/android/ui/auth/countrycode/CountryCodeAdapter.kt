01f0fd73c1c7dd32a96f8b1e519819930b15ee10
Moronke <moronkeanifowose1@gmail>
2022-05-18 15:32:43 +0100

    Kenyan number authentication support

---
+++ app/src/main/java/com/simplifyd/zerodata/android/ui/auth/countrycode/CountryCodeAdapter.kt
@@ -0,0 +1,58 @@
+package com.simplifyd.zerodata.android.ui.auth.countrycode
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.simplifyd.zerodata.android.R
+import com.simplifyd.zerodata.android.utils.AutoUpdateRecyclerView
+import kotlinx.android.synthetic.main.item_country_code.view.*
+import kotlin.properties.Delegates
+
+class CountryCodeAdapter(
+    private val onCountryClicked: (CountryData) -> Unit
+) : RecyclerView.Adapter<CountryCodeAdapter.CountryCodeViewHolder>(),
+    AutoUpdateRecyclerView {
+
+    private var selectedItemPosition = -1
+
+    var countryCodes: List<CountryData> by Delegates.observable(emptyList()) { _, _, _ ->
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryCodeViewHolder {
+        return CountryCodeViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_country_code, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = countryCodes.size
+
+    override fun onBindViewHolder(holder: CountryCodeViewHolder, position: Int) {
+        holder.bind(countryCodes[position], position)
+    }
+
+    inner class CountryCodeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(countryData: CountryData, position: Int) {
+
+            itemView.checkImage.isVisible = position==selectedItemPosition
+
+            itemView.context.getString(countryData.countryName).also { itemView.countryName.text = it }
+            itemView.context.getString(countryData.countryCode).also { itemView.countryCodeTV.text = "+${it}"}
+            itemView.flagImage.setImageResource(countryData.flagImageResourceId)
+
+            itemView.countryCodeLayout.setOnClickListener {
+                selectedItemPosition = position
+                onCountryClicked(countryData)
+                notifyDataSetChanged()
+            }
+
+
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/simplifyd/zerodata/android/ui/auth/countrycode/CountryCodeDialog.kt
+++ app/src/main/java/com/simplifyd/zerodata/android/ui/auth/countrycode/CountryCodeDialog.kt
@@ -1,23 +1,39 @@
 package com.simplifyd.zerodata.android.ui.auth.countrycode
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.simplifyd.zerodata.android.R
 import com.simplifyd.zerodata.android.common.BaseRoundedBottomSheetDialogFragment
+import com.simplifyd.zerodata.android.utils.showToast
 import kotlinx.android.synthetic.main.dialog_country_code.*
 
-class CountryCodeDialog:BaseRoundedBottomSheetDialogFragment() {
+class CountryCodeDialog:BaseRoundedBottomSheetDialogFragment(), (CountryData) -> Unit {
+
+    private val countryCodeAdapter by lazy { CountryCodeAdapter(this)}
     override fun getLayoutRes(): Int = R.layout.dialog_country_code
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        country_code_layout.setOnClickListener {
+        countryCodeRecyclerView.adapter = countryCodeAdapter
+        countryCodeAdapter.countryCodes = countryList
 
-            this.dismiss()
 
-        }
+    }
 
+    companion object{
+
+        val countryList = listOf(
+            CountryData(1, R.string.nigeria, R.string.nigeria_code, R.drawable.nigerian_flag),
+            CountryData(2, R.string.kenya, R.string.kenya_code, R.drawable.kenya_flag)
+        )
+    }
+
+    override fun invoke(countryData: CountryData) {
+        Handler().postDelayed({ findNavController().navigate(CountryCodeDialogDirections.actionCountryCodeDialogToNavigationSignIn(getString(countryData.countryCode)))
+            this.dismiss()}, 500)
 
     }
 }
