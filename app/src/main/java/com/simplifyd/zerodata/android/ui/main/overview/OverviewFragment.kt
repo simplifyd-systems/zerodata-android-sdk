5e54c19a592ad2af076dbb52fa3f7785fd41fba9
Moronke <moronkeanifowose1@gmail>
2022-05-20 13:18:56 +0100

    ChronometerTimer

--- app/src/main/java/com/simplifyd/zerodata/android/ui/main/overview/OverviewFragment.kt
+++ app/src/main/java/com/simplifyd/zerodata/android/ui/main/overview/OverviewFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.os.SystemClock
 import android.util.DisplayMetrics
 import android.view.View
 import android.widget.CompoundButton
@@ -41,62 +42,6 @@ class OverviewFragment : Fragment(R.layout.fragment_dashboard), VpnStatus.StateL
     DisconnectDialogFragment.Companion.Callback {
 
     private val viewModel by viewModels<OverviewViewModel>()
-    private val mainViewModel by activityViewModels<MainViewModel>()
-
-    private var sec = 0
-    private var isRunning = false
-    private var wasRunning = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (savedInstanceState != null) {
-            sec = savedInstanceState.getInt("seconds")
-            isRunning = savedInstanceState.getBoolean("running")
-            wasRunning = savedInstanceState.getBoolean("wasRunning")
-        }
-
-        startTimer()
-    }
-
-
-    fun startTimer(){
-            val customHandler = Handler()
-
-        customHandler.post(object : Runnable {
-            override fun run() {
-                val hoursVar = sec / 3600
-                val minuteVar = sec % 3600 / 60
-                val secVar = sec % 60
-                val timeValue = String.format(
-                    Locale.getDefault(),
-                    "%02d:%02d",
-                    minuteVar,
-                    secVar
-                )
-                if(connection_time_val !=null)
-                    connection_time_val.text = timeValue
-                if (isRunning) {
-                    sec++
-                }
-                customHandler.postDelayed(this, 1000)
-            }
-        })
-    }
-
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        super.onSaveInstanceState(savedInstanceState)
-        savedInstanceState.putInt("seconds", sec)
-        savedInstanceState.putBoolean("running", isRunning)
-        savedInstanceState.putBoolean("wasRunning", wasRunning)
-    }
-
-     override fun onPause() {
-        super.onPause()
-        wasRunning = isRunning
-        isRunning = false
-    }
-
-
 
     private val checkChangedListener =
         CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
@@ -138,7 +83,6 @@ class OverviewFragment : Fragment(R.layout.fragment_dashboard), VpnStatus.StateL
 
                 findNavController().navigate(R.id.action_navigation_overview_to_navigation_notification)
 
-
             }
         }
 
@@ -162,9 +106,6 @@ class OverviewFragment : Fragment(R.layout.fragment_dashboard), VpnStatus.StateL
     override fun onResume() {
         super.onResume()
         VpnStatus.addStateListener(this)
-        if (wasRunning) {
-            isRunning = true
-        }
     }
 
 
@@ -199,24 +140,7 @@ class OverviewFragment : Fragment(R.layout.fragment_dashboard), VpnStatus.StateL
                     showRetrySnackBar(it.error.localizedMessage) { }
                 }
                 is Status.Success -> {
-//                    help_text.text = Html.fromHtml(
-//                        getString(
-//                            R.string.data_count_text,
-//                            it.data.topDomains?.values?.sum() ?: 0
-//                        )
-//                    )
 
-//                    val image: Drawable =
-//                        requireContext().resources.getDrawable(R.drawable.ic_shield_protected)
-//                    val h: Int = image.intrinsicHeight
-//                    val w: Int = image.intrinsicWidth
-//                    image.setBounds(0, 0, w, h)
-//                    help_text.setCompoundDrawablesRelative(
-//                        null,
-//                        image,
-//                        null,
-//                        null
-//                    )
                 }
             }
         }
@@ -333,7 +257,7 @@ class OverviewFragment : Fragment(R.layout.fragment_dashboard), VpnStatus.StateL
                 connect_switch.isEnabled = true
                 if (progressBar != null)
                     progressBar.isVisible = false
-                isRunning = true
+                connection_time_val.start()
                 connect_switch.setOnCheckedChangeListener(checkChangedListener)
                 viewModel.connectUrl?.let {
                     if (!PreferenceManager.getIsSeen()) {
@@ -375,8 +299,8 @@ class OverviewFragment : Fragment(R.layout.fragment_dashboard), VpnStatus.StateL
                 connect_switch.isEnabled = true
                 if (progressBar != null)
                     progressBar.isVisible = false
-                isRunning = false
-                sec = 0
+                connection_time_val.stop()
+                connection_time_val.base = SystemClock.elapsedRealtime()
                 PreferenceManager.setIsSeen(false)
                 connect_switch.setOnCheckedChangeListener(checkChangedListener)
             }
