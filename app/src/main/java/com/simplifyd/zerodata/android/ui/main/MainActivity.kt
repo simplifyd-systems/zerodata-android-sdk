71cf7ac9236c3ebe08e2a1afb7ae1618b48c4c50
Moronke <moronkeanifowose1@gmail>
2022-06-09 15:49:32 +0100

    app update manager update

--- app/src/main/java/com/simplifyd/zerodata/android/ui/main/MainActivity.kt
+++ app/src/main/java/com/simplifyd/zerodata/android/ui/main/MainActivity.kt
@@ -2,11 +2,14 @@ package com.simplifyd.zerodata.android.ui.main
 
 import android.app.Activity
 import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.toColor
 import androidx.core.view.isGone
 import androidx.core.view.isInvisible
@@ -16,6 +19,7 @@ import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.install.InstallState
 import com.google.android.play.core.install.InstallStateUpdatedListener
@@ -56,8 +60,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), InstallStateUpda
         }
 
         observeData()
-        appUpdateManager.registerListener(this)
-        checkUpdates()
+
     }
 
     private fun observeData() {
@@ -118,9 +121,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), InstallStateUpda
         appUpdateManager
             .appUpdateInfo
             .addOnSuccessListener { appUpdateInfo ->
-                if (appUpdateInfo.updateAvailability()
-                    == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
-                ) {
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
                     // If an in-app update is already running, resume the update.
                     appUpdateManager.startUpdateFlowForResult(
                         appUpdateInfo,
@@ -128,14 +129,27 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), InstallStateUpda
                         this,
                         MY_REQUEST_CODE
                     )
+                }else if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED){
+                    popupSnackbarForCompleteUpdate()
                 }
             }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        appUpdateManager.unregisterListener(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        appUpdateManager.registerListener(this)
+        checkUpdates()
     }
 
     fun checkUpdates(){
         val appUpdateInfoTask = appUpdateManager?.appUpdateInfo
         // Checks that the platform will allow the specified type of update.
-        appUpdateInfoTask?.addOnSuccessListener { appUpdateInfo ->
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
             if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE && (appUpdateInfo.clientVersionStalenessDays() ?: -1) >= 7
                 && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
                 // Request the update.
@@ -149,8 +163,22 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), InstallStateUpda
                     this,
                     // Include a request code to later monitor this update request.
                     MY_REQUEST_CODE)
-            } else {
+            } else if((appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE)
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+
+                try {
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        IMMEDIATE,
+                        this,
+                        MY_REQUEST_CODE
+                    )
+                } catch (e: IntentSender.SendIntentException) {
+                    e.printStackTrace()
+                }
+            }else{
                 Log.d("No Update available", "No Update available")
+
             }
         }
     }
@@ -161,11 +189,28 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), InstallStateUpda
             // and request user confirmation to restart the app.
             Log.d("Update Downloaded","An update has been downloaded")
 
-            appUpdateManager.completeUpdate()
+            popupSnackbarForCompleteUpdate()
+
 
             appUpdateManager.unregisterListener(this)
 //            showSnackBarForCompleteUpdate()
+        } else if (p0.installStatus() == InstallStatus.INSTALLED){
+            appUpdateManager.unregisterListener(this)
+
+        } else {
+            Log.i("InstallStateUpdatedListener: state: ", p0.installStatus().toString())
         }
+    }
+
+    private fun popupSnackbarForCompleteUpdate() {
+
+        val snackbar = Snackbar.make(
+           coordinatorLayout,
+            "An update has just been downloaded.",
+            Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("RESTART") { appUpdateManager.completeUpdate() }
+        snackbar.setActionTextColor(ContextCompat.getColor(this, R.color.accent))
+        snackbar.show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -175,17 +220,21 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), InstallStateUpda
             when (resultCode) {
                 Activity.RESULT_OK -> {
                     Log.d("", "Result Ok")
-                    //  handle user's approval }
+                    //handle user's approval
                 }
                 Activity.RESULT_CANCELED -> {
 
                     Log.d("","Result Cancelled")
-                    //  handle user's rejection  }
+                    //handle user's rejection
                 }
                 ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
                     //if you want to request the update again just call checkUpdate()
                     Log.d("", "Update Failure")
-                    //  handle update failure
+                    Toast.makeText(this,
+                        "App Update failed, please try again on the next app launch.",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                    //handle update failure
                 }
             }
         }
