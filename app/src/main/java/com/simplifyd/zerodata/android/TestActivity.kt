70a18687fb0999faede0ae34a9f86a7291c197bf
Moronke <moronkeanifowose1@gmail>
2023-05-25 15:27:49 +0100

    ZSDK - Code clean up and use of ZeroDataSDK

--- app/src/main/java/com/simplifyd/zerodata/android/TestActivity.kt
+++ app/src/main/java/com/simplifyd/zerodata/android/TestActivity.kt
@@ -3,58 +3,68 @@ package com.simplifyd.zerodata.android
 import android.content.Intent
 import android.os.Bundle
 import android.widget.CompoundButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.simplifyd.zerodata.ZeroDataSDK
 import com.simplifyd.zerodata.settings.ZeroDataSDKSettings
 import com.simplifyd.zerodata.settings.ZeroDataStateListener
 import de.blinkt.openvpn.core.ConnectionStatus
 import kotlinx.android.synthetic.main.activity_test.*
-import java.util.logging.Level
+import kotlinx.coroutines.*
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class TestActivity : AppCompatActivity(), ZeroDataStateListener {
-    var isTurnedOn = false
+
+    private val zerodataSDK: ZeroDataSDK by lazy { ZeroDataSDK(application, this) }
+
+    private val scope = CoroutineScope(Dispatchers.IO + Job())
 
     private val checkChangedListener =
-        CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+        CompoundButton.OnCheckedChangeListener { _, isChecked ->
             if (isChecked.not()) {
                 connect_switch.isChecked = false
-                ZeroDataSDK(this).disconnectZeroData()
+                zerodataSDK.disconnectFromZerodata(this)
 
             } else {
-                ZeroDataSDK(this).connectZeroData()
+                zerodataSDK.connectToZerodata(this)
                 connect_switch.isChecked = true
             }
         }
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-        ZeroDataSDK(this).configure(ZeroDataSDKSettings())
-
+        zerodataSDK.configure(ZeroDataSDKSettings("2347234567890"))
         connect_switch.setOnCheckedChangeListener(checkChangedListener)
+    }
 
-
-        btnSubmit.setOnClickListener {
-            if (isTurnedOn) {
-                ZeroDataSDK(this).disconnectZeroData()
-                btnSubmit.text = "Turn on"
-                isTurnedOn = false
-
-
-            } else {
-                ZeroDataSDK(this).connectZeroData()
-
-                btnSubmit.text = "Connecting.."
-                isTurnedOn = true
-
-
-            }
-
+    private fun handleError(error: String) {
+        scope.launch {
+            enableSwitch(false)
+            Toast.makeText(this@TestActivity, error, Toast.LENGTH_SHORT).show()
         }
+    }
 
+    private fun enableSwitch(enable: Boolean) {
+        scope.launch {
+            connect_switch.isEnabled = enable
+        }
+    }
 
+    override fun initializationSuccess() {
+        enableSwitch(true)
+    }
+
+    override fun initializationFailed(error: String) {
+        handleError(error)
+    }
+
+    override fun configureSuccess() {
+        enableSwitch(true)
+    }
+
+    override fun configureFailed(error: String) {
+        handleError(error)
     }
 
     override fun updateState(
@@ -63,15 +73,7 @@ class TestActivity : AppCompatActivity(), ZeroDataStateListener {
         localizedResId: Int,
         level: ConnectionStatus?,
         Intent: Intent?
-    ) {
-        if (level == ConnectionStatus.LEVEL_NOTCONNECTED){
+    ) {}
 
-        }
-    }
-
-    override fun setConnectedVPN(uuid: String?) {
-        TODO("Not yet implemented")
-    }
-
-
+    override fun setConnectedVPN(uuid: String?) {}
 }
